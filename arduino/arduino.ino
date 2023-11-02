dc559f9d773ba8e009239cffad153e314a0d2ef2
Annajosefa <you@example.com>
2023-11-03 00:35:51 +0800

    initial commit

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,42 +1,47 @@
+#include <dht.h> 
+#include "Arduino.h" 
+#include <Wire.h> 
+#include <BH1750.h> 
+  
+  
+dht DHT; 
+BH1750 lightMeter; 
+  
+int dhtPin = 6; 
+int proximitySensor1 = 7; 
+int proximitySensor2 = 8; 
+int proximitySensor3 = 9; 
+int proximitySensor4 = 10; 
+int proximitySensor5 = 11; 
+int lightPin = 3; 
+int fanPin = 4; 
+int sprinklerPin = 5; 
+ 
+const int sensorPin1 = A0; 
+const int sensorPin2 = A1; 
+const int sensorPin3 = A2; 
+const int sensorPin4 = A3; 
+    
+String command = ""; 
+int current_command = -1; 
+  
+void setup() { 
+  Serial.begin(9600); 
+  pinMode(proximitySensor1, INPUT_PULLUP); 
+  pinMode(proximitySensor2, INPUT_PULLUP); 
+  pinMode(proximitySensor3, INPUT_PULLUP); 
+  pinMode(proximitySensor4, INPUT_PULLUP); 
+  pinMode(proximitySensor5, INPUT_PULLUP); 
+  pinMode(fanPin, OUTPUT); 
+  pinMode(sprinklerPin, OUTPUT); 
+ 
+  pinMode (A0, INPUT); 
+  Wire.begin(); 
+  lightMeter.begin(); 
 
-tintin
-#include <dht.h> 
-#include "Arduino.h" 
-#include <Wire.h> 
-#include <BH1750.h> 
-  
-  
-dht DHT; 
-BH1750 lightMeter; 
-  
-int dhtPin = 6; 
-int proximitySensor1 = 7; 
-int proximitySensor2 = 8; 
-int proximitySensor3 = 9; 
-int proximitySensor4 = 10; 
-int proximitySensor5 = 11; 
-int lightPin = 3; 
-int fanPin = 4; 
-int sprinklerPin = 5; 
- 
-const int sensorPin = A0; 
-  
-String command = ""; 
-int current_command = -1; 
-  
-void setup() { 
-  Serial.begin(9600); 
-  pinMode(proximitySensor1, INPUT_PULLUP); 
-  pinMode(proximitySensor2, INPUT_PULLUP); 
-  pinMode(proximitySensor3, INPUT_PULLUP); 
-  pinMode(proximitySensor4, INPUT_PULLUP); 
-  pinMode(proximitySensor5, INPUT_PULLUP); 
-  pinMode(fanPin, OUTPUT); 
-  pinMode(sprinklerPin, OUTPUT); 
- 
-  pinMode (A0, INPUT); 
-  Wire.begin(); 
-  lightMeter.begin(); 
+
+  scale.set_scale(calibration_factor);
+  scale.tare();
 } 
  
  
@@ -50,46 +55,40 @@ void loop(){
     current_command = -1; 
   } 
  
-  float temperature = getTemperature(); 
-  if  (temperature >= 33)   { 
-    digitalWrite(fanPin, HIGH); 
-  } 
-  else  { 
-    digitalWrite(fanPin, LOW); 
-  } 
+ else if (current_command ==1){
+  turnonfan();
+  current_command = -1;
+  }
+
+ else if(current_command == 2){
+  turnofffan();
+  current_command =-1;
+  }
+
+  else if(cuurent_command == 3){
+    turnonsprinkler();
+    current_command= -1;
+  }
+
+  else if(current_command == 4){
+    turnoffsprinkler();
+    current_command= -1;
+  }
+
+  else if(current_command== 5){
+    getWeight();
+    Serialprinln(getWeight());
+    current_command = -1;
+  }
+} 
+
  
-  float MoisturePercentage =  getMoisturePercentage(); 
-  if (MoisturePercentage < 30){ 
-    digitalWrite(sprinklerPin, LOW); 
-  } 
-  else  if(MoisturePercentage >= 60) { 
-    digitalWrite(sprinklerPin, HIGH); 
-  } 
-} 
-void sendResponse(String response){ 
-  /* 
-   * Send response to the Raspberry Pi 
-   */ 
-  Serial.println(response);     
-} 
-  
-void receiveCommand(){ 
-  /* 
-   * Get and return command from Raspberry Pi 
-   */ 
-  if(Serial.available()){ 
-    int sent = Serial.readStringUntil('\n').toInt(); 
-    current_command = sent;    
-  } 
-} 
- 
-void getConditions(){ 
+void sendState(){ 
   DHT.read22(dhtPin); 
-  String condition = String(getTemperature()) + " " + String(getHumidity()) + " " + String(getMoisturePercentage()) + " " + String(getLux()) + " " + String(getProximitySensor1()) + " " + String(getProximitySensor2()) + " " + String(getProximitySensor3()) + " " + String(getProximitySensor4()) + " " + String(getProximitySensor5()); 
+  String condition = String(getTemperature()) + " " + String(getHumidity()) + " " + String(getAverageMoisturePercentage()) + " " + String(getLux()) + " " + String(getProximitySensor1()) + " " + String(getProximitySensor2()) + " " + String(getProximitySensor3()) + " " + String(getProximitySensor4()) + " " + String(getProximitySensor5()); 
   sendResponse(condition); 
- 
-  sendResponse("92"); 
-  Serial.println(condition); 
+  
+  Serial.println(message); 
  
 } 
   
@@ -111,14 +110,18 @@ float getHumidity(){
    
 }  
   
-float getMoisturePercentage (){ 
+float getAverageMoisturePercentage (){ 
   /* 
    * Get current moisture level 
    */ 
   int sensorAnalog; 
-  sensorAnalog = analogRead(sensorPin); 
-  float MoisturePercentage = ( 100 - ( (sensorAnalog/1023.00) * 100) );0; 
-  return MoisturePercentage; 
+  sensorAnalog = {analogRead(sensorPin1), analogRead(sensorPin2), analogRead(sensorPin3), analogRead(sensorPin4)};
+  float totalPercentage = 0;
+for (int i=0; i<4; i++) {
+  totalPercentage += ( 100 - ( (sensorAnalog/1023.00) * 100) );
+}
+  float averageMoisturePercentage = total percentage/4;
+  return averageMoisturePercentage; 
 } 
   
 int getProximitySensor1(){ 
@@ -130,200 +133,6 @@ int getProximitySensor1(){
 } 
  
 int getProximitySensor2(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-======= 
- int getProximitySensor2(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 2 
->>>>>>> cefd6f0b6cb96a651ad7d0dff768de5faefb3e94 
-   */ 
-  int state = digitalRead(proximitySensor2); 
-  return state; 
-} 
- 
- 
-int getProximitySensor3(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-======= 
-  
- int getProximitySensor3(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 3 
->>>>>>> cefd6f0b6cb96a651ad7d0dff768de5faefb3e94 
-   */ 
-  int state = digitalRead(proximitySensor3); 
-  return state; 
-} 
- 
-int getProximitySensor4(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-   */ 
-  int state = digitalRead(proximitySensor4); 
-  return state; 
-} 
- 
- 
-int getProximitySensor5(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-======= 
-  
-int getProximitySensor5(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 5 
->>>>>>> cefd6f0b6cb96a651ad7d0dff768de5faefb3e94 
-   */ 
-  int state = digitalRead(proximitySensor5); 
-  return state; 
-} 
-  
-float getLux (){ 
-  /* 
-   * Get current light level 
-   */ 
- float Lux =
-tintin
-#include <dht.h> 
-#include "Arduino.h" 
-#include <Wire.h> 
-#include <BH1750.h> 
-  
-  
-dht DHT; 
-BH1750 lightMeter; 
-  
-int dhtPin = 6; 
-int proximitySensor1 = 7; 
-int proximitySensor2 = 8; 
-int proximitySensor3 = 9; 
-int proximitySensor4 = 10; 
-int proximitySensor5 = 11; 
-int lightPin = 3; 
-int fanPin = 4; 
-int sprinklerPin = 5; 
- 
-const int sensorPin = A0; 
-  
-String command = ""; 
-int current_command = -1; 
-  
-void setup() { 
-  Serial.begin(9600); 
-  pinMode(proximitySensor1, INPUT_PULLUP); 
-  pinMode(proximitySensor2, INPUT_PULLUP); 
-  pinMode(proximitySensor3, INPUT_PULLUP); 
-  pinMode(proximitySensor4, INPUT_PULLUP); 
-  pinMode(proximitySensor5, INPUT_PULLUP); 
-  pinMode(fanPin, OUTPUT); 
-  pinMode(sprinklerPin, OUTPUT); 
- 
-  pinMode (A0, INPUT); 
-  Wire.begin(); 
-  lightMeter.begin(); 
-} 
- 
- 
- 
-void loop(){ 
-  if(current_command == -1){ 
-    receiveCommand(); 
-  } 
-  else if(current_command == 0){ 
-    getConditions(); 
-    current_command = -1; 
-  } 
- 
-  float temperature = getTemperature(); 
-  if  (temperature >= 33)   { 
-    digitalWrite(fanPin, HIGH); 
-  } 
-  else  { 
-    digitalWrite(fanPin, LOW); 
-  } 
- 
-  float MoisturePercentage =  getMoisturePercentage(); 
-  if (MoisturePercentage < 30){ 
-    digitalWrite(sprinklerPin, LOW); 
-  } 
-  else  if(MoisturePercentage >= 60) { 
-    digitalWrite(sprinklerPin, HIGH); 
-  } 
-} 
-void sendResponse(String response){ 
-  /* 
-   * Send response to the Raspberry Pi 
-   */ 
-  Serial.println(response);     
-} 
-  
-void receiveCommand(){ 
-  /* 
-   * Get and return command from Raspberry Pi 
-   */ 
-  if(Serial.available()){ 
-    int sent = Serial.readStringUntil('\n').toInt(); 
-    current_command = sent;    
-  } 
-} 
- 
-void getConditions(){ 
-  DHT.read22(dhtPin); 
-  String condition = String(getTemperature()) + " " + String(getHumidity()) + " " + String(getMoisturePercentage()) + " " + String(getLux()) + " " + String(getProximitySensor1()) + " " + String(getProximitySensor2()) + " " + String(getProximitySensor3()) + " " + String(getProximitySensor4()) + " " + String(getProximitySensor5()); 
-  sendResponse(condition); 
- 
-  sendResponse("92"); 
-  Serial.println(condition); 
- 
-} 
-  
-float getTemperature(){ 
-  /* 
-   * Get current room temperature 
-   */ 
-  float temperature = DHT.temperature; 
-  return temperature; 
- 
-} 
-  
-float getHumidity(){ 
-  /* 
-   * Get current humidity level 
-   */ 
-  float humidity = DHT.humidity; 
-  return humidity; 
-   
-}  
-  
-float getMoisturePercentage (){ 
-  /* 
-   * Get current moisture level 
-   */ 
-  int sensorAnalog; 
-  sensorAnalog = analogRead(sensorPin); 
-  float MoisturePercentage = ( 100 - ( (sensorAnalog/1023.00) * 100) );0; 
-  return MoisturePercentage; 
-} 
-  
-int getProximitySensor1(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-   */ 
-  int state = digitalRead(proximitySensor1); 
-  return state; 
-} 
- 
-int getProximitySensor2(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 1 
-======= 
- int getProximitySensor2(){ 
-  /* 
-   * Check if an object is detected on Proximity Sensor 2 
->>>>>>> cefd6f0b6cb96a651ad7d0dff768de5faefb3e94 
-   */ 
   int state = digitalRead(proximitySensor2); 
   return state; 
 } 
@@ -372,4 +181,32 @@ float getLux (){
    */ 
  float Lux =lightMeter.readLightLevel();
  return Lux;
+}
+
+float getWeight(){
+  /*
+  get current weight*/
+  unit = scale.get_units(), 10;
+  if (units<0)
+  {
+    units = 0.00
+  }
+  float final_weight= (units * 0.001);
+  return final weight;
+}
+
+void turnonfan(){
+  digitalWrite(fanPin, HIGH);
+}
+
+void  turnofffan(){
+  digitalwrite(fanPin,LOW);
+}
+
+void turnonsprinkler(){
+  digitalWrite(sprinklerPin, HIGH);
+}
+
+void turnofffan(){
+  digitalwrite(sprinklerPin, LOW);
 }
---
+++ newmain.py
@@ -0,0 +1,239 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import messaging
+
+import datetime
+import serial
+import time
+
+class OnionSense:
+
+    def __init__(self) :
+         '''
+        Initialize a machine object
+        '''
+         cred = credentials.Certificate('account.json')
+         app = firebase_admin.initialize_app(cred)
+         db = firestore.client()
+         
+         parameters_ref = db.collection('parameters')
+         rows_ref = db.collection('rows')
+         harvests_ref = db.collection('harvests')
+         users_ref= db.collection('users')
+         arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+
+    def send_command(self, command: int):
+        '''
+        Send command to Arduino
+        can be used to explicitly invoke Arduino operation without calling specific functions\n
+
+        Parameters:
+        command(int): Command to send
+        '''
+        if(command in self.available_commands):
+            self.arduino.write(bytes(str(command)+'\n','utf-8'))
+        else:
+            raise Exception('Unknown command')
+    
+    def get_arduino_response(self)->str:
+        '''
+        Get arduino serial response
+
+        Returns:
+        response(str): Arduino response
+        '''
+        try:
+            response = self.arduino.readline().decode('utf-8').rstrip
+        except UnicodeDecodeError:
+            response = self.arduino.readline().decode('utf-8').rstrip
+        return response
+    
+
+    
+    def get_data(self)-> dict:
+        '''
+        Get current data from sensors
+
+        Returns:
+        parameters(dict): Parameters including message indicating succes or fail
+        '''
+        self.send_command(0)
+        response = self.get_arduino_response()
+
+        while not response:
+            self.get_arduino_response()
+
+        data = response.split()
+        try:
+             humidity = float(data[1])
+             soil_moisture = float(data[2])*100
+             lux = float(data[3])
+             proximity_sensor_1 = int(data[4])
+             proximity_sensor_2 = int(data[5])
+             proximity_sensor_3 = int(data[6])
+             proximity_sensor_4 = int(data[7])
+             proximity_sensor_5 = int(data[8])
+             
+             parameters = {
+                 'soil': soil_moisture,
+                 'humidity': humidity,
+                 'temperature': temperature,
+                 'light': lux,
+                 'r1': (proximity_sensor_1),
+                 'r2': (proximity_sensor_2),
+                 'r3': (proximity_sensor_3),
+                 'r4': (proximity_sensor_4),
+                 'r5': (proximity_sensor_5),
+                 'success': True
+             }
+             
+        except:
+            parameters = {
+             'success':False
+             }
+        return parameters
+    
+
+
+    def update_parameters(self, parameters:dict):
+        '''
+        Add new parameter entry in firebase
+
+        Parameters:
+        parameters(dict): Dict containing parameters\n
+        Sample format:
+            parameters = {\n
+            'soil': soil_moisture, \n
+            'humidity': humidity, \n
+            'temperature': temperature, \n
+            'light': lux, \n
+            }
+        '''
+        data = {
+            'soil': parameters['soil'],
+            'humidity':parameters['humidity'],
+            'temperature': parameters['temperature'],
+            'light': parameters ['lux'],
+            'created_at': datetime.datetime.now(tz=datetime.timezone.utc)
+        }
+        self.parameter_reference.add(data)
+
+
+
+    def update_harvest_rows(self, parameters: dict):
+        '''
+        [To Update]
+
+        Add/ update harvest rows entry in firebase
+
+        Parameters:
+        parameters(dict): Dict containing harvest rows \n
+        Sample format:
+            parameters = {\n
+                'r1': (proximity_sensor_1),
+                'r2': (proximity_sensor_2),
+                'r3': (proximity_sensor_3),
+                'r4': (proximity_sensor_4),
+                'r5': (proximity_sensor_5),
+        }
+        '''
+
+        data = {
+            'r1': parameters[proximity_sensor_1],
+            'r2': parameters[proximity_sensor_2],
+            'r3': parameters[proximity_sensor_3],
+            'r4': parameters[proximity_sensor_4],
+            'r5': parameters[proximity_sensor_5],
+            'created_at': datetime.datetime.now(tz=datetime.timezone.utc)
+        }
+        self.row_reference.add(data)
+
+
+
+    def add_harvest(self, amount:float):
+        '''
+        Add a new harvest entry in firebase
+
+        Parameters:
+        amount(float): Amount of harvest in kg
+        '''
+        data = {
+            'amount': amount,
+            'created_at': datetime.datetime.now(tz=datetime.timezone.utc)
+        }
+        self.parameter_reference.add(data)
+    
+
+
+    def _get_user_token(self)-> list:
+        '''
+        Get all existing user tokens from firebase
+
+        Returns:
+        tokens (list): List of all retrieved tokens
+        '''
+        tokens = []
+        users = self.user_reference.stream()
+        for user in users:
+            tokens.append(user.id)
+        return tokens
+    
+
+    def send_notification(self, title: str, body: str):
+        '''
+        Send a notification to all users
+
+        Parameters:
+        title(str): Title message
+        body(str): Message content
+        '''
+        message = messaging.MulticastMessage(
+            notification = messaging.Notification(
+                title = title,
+                body= body,
+            ),
+            tokens = self._get_user_tokens()
+        )
+
+    
+    def turn_on_fan(self):
+        '''
+        Explicit function calling turnOnFan in arduino
+        '''
+        self.send_command(1)
+
+
+    def turn_off_fan(self):
+        '''
+        Explicit function calling turnOffFan in arduino
+        '''
+        self.send_command(2)
+
+    def turn_on_sprinkler(self):
+        '''
+        Explicit function turnOnSprinkler in arduino
+        '''
+        self.send_command(3)
+
+    def turn_off_sprinkler(self):
+        '''
+        Explicit function turnOffSprinkler
+        '''
+        self.send_command(4)
+
+    def get_internet_datetime(self)-> datetime.datetime:
+        '''
+        Get datetime from internet
+
+        Returns:
+        datetime(datetime.datetime): Current datetime
+        '''
+        response = urlopen('http://just-the-time.appspot.com/')
+        datetime_str = response.read().strip().decode('utf-8')
+        _datetime = datetime.datetime.striptime(datetime_str,'%Y-%m-%d %H:%M:%S')
+        _datetime = datetime + datetime.timedelta(hours = 8)
+        return _datetime
+    
+
+ 
