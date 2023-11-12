3a2fafc2c4d62d4f25ea1ac8359e62b53648ec8e
Annajosefa <annalogmao17@gmail.com>
2023-11-13 03:26:39 +0800

    main

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
                     body= 'Harvest is ready in Row 2. Please collect!'
                 )
                 notification_ready_2 = False
-                last_notification_2 = datetime.date.now()
+                last_notification_2 = datetime.datetime.now()
 
             if not notification_ready_2 \
                 and (datetime.datetime.now() - last_notification_2) >= datetime.timedelta(minutes=30):
@@ -107,7 +107,7 @@ if __name__ == '__main__':
                     body= 'Harvest is ready in Row 3. Please collect!'
                 )
                 notification_ready_3 = False
-                last_notification_3 = datetime.date.now()
+                last_notification_3 = datetime.datetime.now()
 
             if not notification_ready_3 \
                 and (datetime.datetime.now() - last_notification_3) >= datetime.timedelta(minutes=30):
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                     body= 'Harvest is ready in Row 4. Please collect!'
                 )
                 notification_ready_4 = False
-                last_notification_4 = datetime.date.now()
+                last_notification_4 = datetime.datetime.now()
 
             if not notification_ready_4 \
                 and (datetime.datetime.now() - last_notification_4) >= datetime.timedelta(minutes=30):
@@ -131,7 +131,7 @@ if __name__ == '__main__':
                     body= 'Harvest is ready in Row 5. Please collect!'
                 )
                 notification_ready_5 = False
-                last_notification_5 = datetime.date.now()
+                last_notification_5 = datetime.datetime.now()
 
             if not notification_ready_5 \
                 and (datetime.datetime.now() - last_notification_5) >= datetime.timedelta(minutes=30):
